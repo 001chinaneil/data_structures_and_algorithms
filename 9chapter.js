e5ad0401a58890c4428c622f9f723364aaea69cc
zhangguobiao <zhangguobiao@zuoyebang.com>
2020-03-09 22:32:58 +0800

    判断子集 & 求补集

--- 9chapter.js
+++ 9chapter.js
@@ -4,12 +4,12 @@ function Set(){
     this.dataStore = [];
     this.add = add;
     this.remove = remove;
-    // this.size = size;
+    this.size = size;
     this.contains = contains;
     this.union = union;// 并集
     this.intersect = intersect;// 交集
-    // this.subset = subset;
-    // this.difference = difference;
+    this.subset = subset;
+    this.difference = difference;// 求补集
     this.show = show;
 }
 
@@ -69,6 +69,34 @@ function intersect(set){
     return tempSet;
 }
 
+function size(){
+    return this.dataStore.length;
+}
+
+function subset(set){// 是否是子集
+    if( this.size() > set.size() ){
+        return false;
+    }else {
+        for(var i = 0; i < this.dataStore; ++i){
+            if( !set.contains(this.dataStore[i]) ){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+function difference(set){
+    var tempSet = new Set();
+    for(var i = 0; i < this.dataStore.length; ++i){
+        if( !set.contains(this.dataStore[i]) ){
+            tempSet.add(this.dataStore[i]);
+        }
+    }
+    return tempSet;
+}
+
+
 console.log('=====测试 使用Set类====================');
 // var names = new Set();
 // names.add('David');
@@ -113,3 +141,36 @@ console.log(it.show());
 console.log('=====求两个集合的交集===================================================');
 var inter = cis.intersect(dmp);
 console.log(inter.show());
+
+console.log('=====判断一个集合是否是另一个集合的子集=====================================================');
+var it = new Set();
+it.add('Cynthia');
+it.add('Clayton');
+it.add('Jennifer');
+it.add('Danny');
+it.add('Jonathan');
+it.add('Terrill');
+it.add('Raymond');
+it.add('Mike');
+var dmp = new Set();
+dmp.add('Cynthia');
+dmp.add('Raymond');
+dmp.add('Jonathan');
+if( dmp.subset(it) ){
+    console.log('DMP is a subset of IT');
+}else {
+    console.log('DMP is not a subset of IT');
+}
+
+console.log('=====求两个集合的补集=======================================================');
+var cis = new Set();
+var it = new Set();
+cis.add('Clayton');
+cis.add('Jennifer');
+cis.add('Danny');
+it.add('Bryan');
+it.add('Clayton');
+it.add('Jennifer');
+var diff = new Set();
+diff = cis.difference(it);
+console.log('补集是：', diff.show());
